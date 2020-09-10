e1b5cc39402962c72676bdfd74999b870fd24ac6
eRqa <ivlasov.spb@gmail.com>
2020-09-10 21:43:36 +0300

    Class Calculator added

---
+++ src/ru/job4j/Calculator.java
@@ -0,0 +1,28 @@
+package ru.job4j;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        int first = 1;
+        int second = 2;
+        int result = first + second;
+        System.out.println(result);
+        System.out.println("-------------------");
+
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int two = 2;
+
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
+
+        System.out.println("sixDivTwo = " + sixDivTwo);
+        System.out.println("fiveMinusTwo = " + fiveMinusTwo);
+        System.out.println("fourTimeTwo = " + fourTimeTwo);
+
+    }
+
+}
