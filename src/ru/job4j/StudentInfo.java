6fd8d693f90ff5fefd93225f1a4ef86e1c058370
eRqa <ivlasov.spb@gmail.com>
2020-09-10 20:48:20 +0300

    Вывод таблицы умножения

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Pavel Vlasov");
+        System.out.println("28.05.1998");
+    }
+}
