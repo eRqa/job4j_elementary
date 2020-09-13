135169b8ffd148dcd61a28f9d2d7282b86200662
eRqa <ivlasov.spb@gmail.com>
2020-09-13 18:41:08 +0300

    Описаны автотесты на класс Converter

---
+++ test/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.converter;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import static org.junit.Assert.*;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        int in = 140;
+        int expected = 2;
+
+        int out = Converter.rubleToDollar(in);
+
+        Assert.assertEquals(expected, out);
+
+    }
+
+    @Test
+    public void whenConvert210RblThen3Euro() {
+        int in = 210;
+        int expected = 3;
+
+        int out = Converter.rubleToDollar(in);
+
+        Assert.assertEquals(expected, out);
+
+    }
+
+    @Test
+    public void whenConvert120RblThen2Dollar() {
+
+        int in = 120;
+        int expected = 2;
+
+        int out = Converter.rubleToDollar(in);
+
+        Assert.assertEquals(expected, out);
+
+    }
+}
