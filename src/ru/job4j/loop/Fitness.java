818daead998f31299c7ec1d4bdad9619850dd130
eRqa <ivlasov.spb@gmail.com>
2020-09-23 21:03:30 +0300

    Реализация задачи 359389

---
+++ src/ru/job4j/loop/Fitness.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+public class Fitness {
+
+    public static int calc(int ivan, int nik) {
+
+        int counter = 0;
+
+        while (ivan < nik) {
+
+            ivan *= 3;
+            nik *= 2;
+
+            counter += 1;
+
+        }
+
+        return counter;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calc(90, 100));
+    }
+
+}
