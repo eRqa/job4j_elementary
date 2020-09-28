96d41710d0f5f5a82fb0373be54bcbc9e66c3cfb
eRqa <ivlasov.spb@gmail.com>
2020-09-28 21:17:04 +0300

    Удалена проверка first >= second && first >= third, в ней нет необходимости так как first уже содержится в result

--- src/ru/job4j/condition/ThreeMax.java
+++ src/ru/job4j/condition/ThreeMax.java
@@ -4,9 +4,7 @@ public class ThreeMax {
 
     public static int max(int first, int second, int third) {
         int result = first;
-        if (first >= second && first >= third) {
-            result = first;
-        }
+
         if (second >= first && second >= third) {
             result = second;
         }
