49e952f0fc696681167ebc0ddb837fe8fbcdae06
eRqa <ivlasov.spb@gmail.com>
2020-09-28 21:37:08 +0300

    Объявление и инициализация массива с разменостью 4, вывод значений в консоль

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -10,5 +10,17 @@ public class ArrayDefinition {
 
         float[] prices = new float[40];
         System.out.println("Размер массив prices = " + prices.length);
+
+        System.out.println("-----------------------------------");
+
+        String[] names = new String[4];
+        names[0] = "Ipolit";
+        names[1] = "Jeka Lukashin";
+        names[2] = "Nadya Shevelyova";
+        names[3] = "Ivan Vasyl'evich";
+
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
