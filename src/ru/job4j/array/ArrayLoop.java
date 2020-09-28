bdd9978fae4506fe8deb99dbf31dabc4c8c1af58
eRqa <ivlasov.spb@gmail.com>
2020-09-28 21:44:00 +0300

    Инициализация и вывод массива

---
+++ src/ru/job4j/array/ArrayLoop.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class ArrayLoop {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            array[i] = (i * 2) + 3;
+        }
+
+        for (int el : array) {
+            System.out.println(el);
+        }
+    }
+}
