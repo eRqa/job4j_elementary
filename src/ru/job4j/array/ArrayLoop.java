b2240738c7f9b25a3225b5e3693a5642aabf8e99
eRqa <ivlasov.spb@gmail.com>
2020-10-01 23:30:27 +0300

    Исправлены паарметры цикла. Итератор меняется не до заданного числа, а до длинны массива

--- src/ru/job4j/array/ArrayLoop.java
+++ src/ru/job4j/array/ArrayLoop.java
@@ -4,7 +4,7 @@ public class ArrayLoop {
     public static void main(String[] args) {
         int[] array = new int[5];
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < array.length; i++) {
             array[i] = (i * 2) + 3;
         }
 
