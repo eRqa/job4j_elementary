d6ca3f0385f5a82a43069e95ea0ec0ab2a5920e0
eRqa <ivlasov.spb@gmail.com>
2020-10-06 22:50:13 +0300

    Метод проверки моно-строки в матрице

---
+++ test/ru/job4j/array/MatrixCheckTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MatrixCheckTest {
+
+    @Test
+    public void whenHasMonoHorizontal() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', 'X', 'X'},
+                {' ', ' ', ' '},
+        };
+        boolean result = MatrixCheck.monoHorizontal(input, 1);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenMonoHorizontal0() {
+        char[][] input = {
+                {'X', 'X', 'X'},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
+        };
+        boolean result = MatrixCheck.monoHorizontal(input, 0);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotMonoHorizontal0() {
+        char[][] input = {
+                {'X', 'X', 'X'},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
+        };
+        boolean result = MatrixCheck.monoHorizontal(input, 2);
+        assertThat(result, is(false));
+    }
+
+}
