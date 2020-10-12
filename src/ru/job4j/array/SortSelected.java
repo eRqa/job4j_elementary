72a2776d8cb94a680f1ffb0a5703d7f6698842cd
eRqa <ivlasov.spb@gmail.com>
2020-10-12 19:53:39 +0300

    Исправлениемеханизма сортировки выборкой

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -5,7 +5,7 @@ public class SortSelected {
 
         for (int i = 0; i < data.length; i++) {
                 int min = MinDiapason.findMin(data, i, data.length - 1);
-                int index = FindLoop.indexOf(data, min, 0, data.length);
+                int index = FindLoop.indexOf(data, min, i, data.length - 1);
                 if (index != -1) {
                     SwitchArray.swap(data, i, index);
                 }
